933e42ee52a2f5eea44a74d2289163815b781d59
Jan Gorjanc <johnee@gmail.com>
2019-02-14 14:42:00 +0100

    Fix example test formatting

--- main_test.go
+++ main_test.go
@@ -7,25 +7,25 @@ func ExampleHelp() {
 	os.Setenv("CP_APPLICATION_SECRET", "test")
 	main()
 
-	/*Output:Usage:
-	  cphalo [command]
-
-	Available Commands:
-	  alert-profiles      Manage alert profiles
-	  csp-accounts        Manage CSP accounts
-	  firewall-interfaces Manage firewall interfaces
-	  firewall-policies   Manage firewall policies
-	  firewall-rules      Manage firewall rules for a specific policy
-	  firewall-services   Manage firewall services
-	  firewall-zones      Manage firewall zones
-	  help                Help about any command
-	  server-groups       Manage server groups
-	  servers             Manage servers
-
-	Flags:
-	  -h, --help      help for cphalo
-	      --verbose   Output verbose information
-
-	Use "cphalo [command] --help" for more information about a command.*/
+	//Output:Usage:
+	//   cphalo [command]
+	//
+	//Available Commands:
+	//   alert-profiles      Manage alert profiles
+	//   csp-accounts        Manage CSP accounts
+	//   firewall-interfaces Manage firewall interfaces
+	//   firewall-policies   Manage firewall policies
+	//   firewall-rules      Manage firewall rules for a specific policy
+	//   firewall-services   Manage firewall services
+	//   firewall-zones      Manage firewall zones
+	//   help                Help about any command
+	//   server-groups       Manage server groups
+	//   servers             Manage servers
+	//
+	//Flags:
+	//   -h, --help      help for cphalo
+	//       --verbose   Output verbose information
+	//
+	//Use "cphalo [command] --help" for more information about a command.
 
 }
